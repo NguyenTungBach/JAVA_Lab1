bd011cf32273e637117f5228751cdbd83d1a8e8d
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-06-11 10:17:34 +0700

    Bài tập Lab 1, Lab 2, Lab 3

--- src/demo/entity/HelperClass.java
+++ src/demo/entity/HelperClass.java
@@ -11,53 +11,37 @@ public class HelperClass {
         return chuvi;
     }
 
+    public int caseLuongThuongThang13(int luong, double soNamCongHien){
+        int luongThang13 = 0;
+        if (soNamCongHien < 1){
+            luongThang13 = ((int) (luong * 0.3)) + luong;
+        }
+        if (soNamCongHien >=1 && soNamCongHien <=2 ){
+            luongThang13 = ((int) (luong * 0.5)) + luong;
+        }
+        if (soNamCongHien >=2 && soNamCongHien <=5 ){
+            luongThang13 = luong + luong;
+        }
+        if (soNamCongHien > 5 ){
+            luongThang13 = (luong * 2)  + luong;
+        }
+        return luongThang13;
+    }
+
     public int tinhLuongThuongThang13(int soNamKinhNghiem, double soNamCongHien){
         int luongThang13 = 0;
         int luong = 0;
         if (soNamKinhNghiem < 2 ){
             luong = 10000000;
-            if (soNamCongHien < 1){
-                luongThang13 = (int) (luong * 0.3);
-            }
-            if (soNamCongHien >=1 && soNamCongHien <=2 ){
-                luongThang13 = (int) (luong * 0.5);
-            }
-            if (soNamCongHien >=1 && soNamCongHien <=2 ){
-                luongThang13 = luong;
-            }
-            if (soNamCongHien > 5 ){
-                luongThang13 = luong * 5;
-            }
+            luongThang13 = caseLuongThuongThang13(luong,soNamCongHien);
         }
         if (soNamKinhNghiem >= 2 && soNamKinhNghiem <= 5){
             luong = 20000000;
-            if (soNamCongHien <1){
-                luongThang13 = (int) (luong * 0.3);
-            }
-            if (soNamCongHien >=1 && soNamCongHien <=2 ){
-                luongThang13 = (int) (luong * 0.5);
-            }
-            if (soNamCongHien >=1 && soNamCongHien <=2 ){
-                luongThang13 = luong;
-            }
-            if (soNamCongHien > 5 ){
-                luongThang13 = luong * 5;
-            }
+            luongThang13 = caseLuongThuongThang13(luong,soNamCongHien);
         }
         if (soNamKinhNghiem > 5 ){
             luong = 30000000;
-            if (soNamCongHien <1){
-                luongThang13 = (int) (luong * 0.3);
-            }
-            if (soNamCongHien >=1 && soNamCongHien <=2 ){
-                luongThang13 = (int) (luong * 0.5);
-            }
-            if (soNamCongHien >=1 && soNamCongHien <=2 ){
-                luongThang13 = luong;
-            }
-            if (soNamCongHien > 5 ){
-                luongThang13 = luong * 5;
-            }
+            luongThang13 = caseLuongThuongThang13(luong,soNamCongHien);
         }
         return luongThang13;
     }
